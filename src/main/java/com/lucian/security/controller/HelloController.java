04d6f3196df3e2602d153db12f1b90cdce26fd04
lucian <17607003651@163.com>
2020-11-17 14:25:34 +0800

    controller 位置错误

--- src/main/java/com/lucian/security/controller/HelloController.java
+++ src/main/java/com/lucian/security/controller/HelloController.java
@@ -1,10 +1,10 @@
-package com.lucian.security;
+package com.lucian.security.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+public class HelloController {
     @GetMapping("hello")
     public String hello(){
         return "hello";
